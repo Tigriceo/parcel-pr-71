8b40f9c8c8386a6f8d850c4c36b81311150a0c79
Tigriceo <koval.ss92@gmail.com>
2022-12-04 12:38:25 +0200

    plagins is added
    https://bluzky.github.io/nice-select2/
    https://imask.js.org/guide.html

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import IMask from 'imask';
+//import NiceSelect from "nice-select2";
+
+
+/**
+ * Nice select
+ */
+const selects = document.querySelectorAll('.js-select');
+for (let i = 0; i < selects.length; i++) {
+    const select = selects[i];
+    NiceSelect.bind(select, {
+        //options
+    });
+}
+
+
+/**
+ * Input mask
+ */
+var element = document.getElementById('selector');
+var maskOptions = {
+    mask: '+{3}(000)000-00-00'
+};
+var mask = IMask(element, maskOptions);
